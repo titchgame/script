6dfcf89576de8e8434688cf6ce110e630c329655
tdameros <tomdamerose@gmail.com>
2025-05-04 12:55:28 +0200

    feat: add gather

--- incarnam.lua
+++ incarnam.lua
@@ -7,16 +7,16 @@ MAX_MONSTERS = 8
 function move()
     return {
         -- Les déplacements, récoltes, combats
-        { map = "-2,-3", changeMap = "top" },
-        { map = "-2,-4", changeMap = "top" },
-        { map = "-2,-5", changeMap = "left" },
-        { map = "-3,-5", changeMap = "top" },
-        { map = "-3,-6", changeMap = "right" },
-        { map = "-2,-6", changeMap = "right" },
-        { map = "-1,-6", changeMap = "bottom" },
-        { map = "-1,-5", changeMap = "bottom" },
-        { map = "-1,-4", changeMap = "bottom" },
-        { map = "-1,-3", changeMap = "left" },
+        { map = "-2,-3", changeMap = "top", gather = true },
+        { map = "-2,-4", changeMap = "top", gather = true},
+        { map = "-2,-5", changeMap = "left", gather = true },
+        { map = "-3,-5", changeMap = "top", gather = true },
+        { map = "-3,-6", changeMap = "right", gather = true },
+        { map = "-2,-6", changeMap = "right", gather = true },
+        { map = "-1,-6", changeMap = "bottom", gather = true },
+        { map = "-1,-5", changeMap = "bottom", gather = true },
+        { map = "-1,-4", changeMap = "bottom", gather = true },
+        { map = "-1,-3", changeMap = "left", gather = true },
     }
 end
 
